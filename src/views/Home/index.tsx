fa5411ec6e0689a6ba850144fe17a4f97034d5c0
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-09-04 21:57:25 +0545

    feat: add toast featur

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/radix/button';
+import { toast } from 'react-toastify';
 const Home = () => {
   return (
     <div>
-      Welcome to Home page
+      <p className="text-[4rem] font-bold">Welcome to Home page 🤠 😎 </p>
+      <Button variant={'default'} onClick={() => toast('Lets Get Started')}>
+        Click Me
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
